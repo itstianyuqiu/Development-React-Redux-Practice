e68f0a06853999658339c998d4b20754e9c24780
tqiu654 <tqiu654@aucklanduni.av.nz>
2021-04-23 10:27:47 +1200

    Hooks/Redux Practice

--- src/index.js
+++ src/index.js
@@ -3,10 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import PracticeContext from './PracticeContext';
+import PracticeToDoList from './PracticeToDoList';
+import PracticeReducer from './PracticeReducer';
+import PracticeMiniRedux from './PracticeMiniRedux';
+import {Provider} from "react-redux"
+import store from './store';
+import Test01Div from './component/test01/Test01Div';
+import Test02Div from './component/test01/Test02Div';
+import TestButton from './component/test01/TestButton';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <PracticeToDoList/>
+    {/* <PracticeContext/> */}
+    {/* <PracticeReducer/> */}
+    {/* <PracticeMiniRedux/> */}
+    {/* <Provider store = {store}>
+      <Test01Div/>
+      <Test02Div/>
+      <TestButton/>
+    </Provider> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
